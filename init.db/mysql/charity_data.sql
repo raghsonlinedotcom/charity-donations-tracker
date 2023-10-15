217825e327ec37a5c39f0acd64fc4cec19e8430c
manikanta <manikanta@dataworkz.io>
2023-10-15 13:47:11 +0530

    [CDT-9] task completed for all the items in the epic(CDT-1 to CDT-8)

---
+++ init.db/mysql/charity_data.sql
@@ -0,0 +1,56 @@
+INSERT INTO charity_donation 
+    (financialYear, date, receipt, amount, institution, address, pan, receipts, remarks)
+VALUES 
+    ('2023-2024', '2023-10-11', 'R123456', 1000.00, 'American Charity Foundation', '123 Main St, City, State, 12345', 'ABC123', 'A123, B456', 'General Donation - Helping those in need'),
+    ('2023-2024', '2023-10-12', 'R123457', 750.00, 'United Relief Organization', '456 Oak St, Town, State, 67890', 'XYZ789', 'C789, D012', 'Emergency Fund - Disaster Relief'),
+    ('2023-2024', '2023-10-15', 'R123458', 500.00, 'Hope for Tomorrow', '789 Pine St, Village, State, 34567', 'PQR456', 'E345, F678', 'Education Support - Scholarships'),
+    ('2023-2024', '2023-11-01', 'R123476', 1200.00, 'Community Aid Foundation', '321 Elm St, Borough, State, 54321', 'LMN789', 'G901, H234', 'Healthcare Programs - Vaccination Drive');
+    ('2023-2024', '2023-10-13', 'UKR123001', 800.00, 'British Aid Society', '10 Downing St, London, SW1A 2AA', 'UKP789', 'UKA123, UKB456', 'Disaster Response - Flood Relief'),
+    ('2023-2024', '2023-10-17', 'UKR123002', 600.00, "London Children's Fund", '20 Baker St, Manchester, M1 3BB', 'UKP234', 'UKC789, UKD012', 'Education Development - School Supplies'),
+    ('2023-2024', '2023-10-20', 'UKR123003', 1000.00, 'Hope UK', '30 Oxford St, Birmingham, B1 3AG', 'UKP567', 'UKE345, UKF678', 'Health and Wellness - Mental Health Awareness'),
+    ('2023-2024', '2023-11-02', 'UKR123004', 400.00, 'Scotland Cares Foundation', '40 Edinburgh St, Edinburgh, EH1 1DH', 'UKP890', 'UKG901, UKH234', 'Community Support - Elderly Care'),
+    ('2023-2024', '2023-11-07', 'UKR123005', 1200.00, 'Welsh Aid Network', '50 Cardiff St, Cardiff, CF10 1BB', 'UKP012', 'UKI567, UKJ890', 'Environmental Conservation - Tree Plantation');
+    ('2023-2024', '2023-10-14', 'NZR123001', 900.00, 'Kiwis for a Cause', '5 Queen St, Auckland, 1010', 'NZP789', 'NZA123, NZB456', 'Child Welfare - Foster Care Support'),
+    ('2023-2024', '2023-10-18', 'NZR123002', 700.00, 'Nature Conservation NZ', '15 Lambton Quay, Wellington, 6011', 'NZP234', 'NZC789, NZD012', 'Wildlife Preservation - Kiwi Protection'),
+    ('2023-2024', '2023-10-21', 'NZR123003', 1100.00, 'Maori Cultural Foundation', '25 Worcester St, Christchurch, 8011', 'NZP567', 'NZE345, NZF678', 'Cultural Heritage - Language Revitalization'),
+    ('2023-2024', '2023-11-03', 'NZR123004', 500.00, 'Healthcare NZ Trust', '35 Willis St, Palmerston North, 4410', 'NZP890', 'NZG901, NZH234', 'Medical Research - Disease Prevention'),
+    ('2023-2024', '2023-11-08', 'NZR123005', 1300.00, 'Aotearoa Green Initiative', '45 George St, Dunedin, 9016', 'NZP012', 'NZI567, NZJ890', 'Sustainable Living - Renewable Energy Projects');
+    ('2023-2024', '2023-10-15', 'AUR123001', 950.00, 'Australian Red Cross', '15 Pitt St, Sydney, NSW 2000', 'AUP789', 'AUA123, AUB456', 'Disaster Relief - Bushfire Assistance'),
+    ('2023-2024', '2023-10-19', 'AUR123002', 850.00, 'Melbourne Youth Foundation', '25 Collins St, Melbourne, VIC 3000', 'AUP234', 'AUC789, AUD012', 'Youth Empowerment - Skill Development'),
+    ('2023-2024', '2023-10-22', 'AUR123003', 1200.00, 'Great Barrier Reef Conservation', '35 Queen St, Brisbane, QLD 4000', 'AUP567', 'AUE345, AUF678', 'Environmental Preservation - Marine Conservation'),
+    ('2023-2024', '2023-11-04', 'AUR123004', 600.00, 'Outback Education Initiative', '45 Mitchell St, Darwin, NT 0800', 'AUP890', 'AUG901, AUH234', 'Rural Education - Remote School Support'),
+    ('2023-2024', '2023-11-09', 'AUR123005', 1400.00, 'Perth Community Care', '55 St Georges Terrace, Perth, WA 6000', 'AUP012', 'AUI567, AUJ890', 'Community Health - Wellness Clinics');
+    ('2023-2024', '2023-10-15', 'AKR123001', 500.00, 'Alaska Community Aid', '123 Northern Lights Blvd, Anchorage, AK 99501', 'AKP789', 'AKA123, AKB456', 'Cold Weather Relief - Warm Clothing Drive'),
+    ('2023-2024', '2023-11-02', 'AKR123002', 300.00, 'Arctic Wildlife Fund', '456 Denali St, Fairbanks, AK 99701', 'AKP234', 'AKC789, AKD012', 'Wildlife Conservation - Protecting Arctic Fauna'),
+    ('2023-2024', '2023-11-08', 'AKR123003', 700.00, 'Northern Lights Education Foundation', '789 Kodiak St, Juneau, AK 99801', 'AKP567', 'AKE345, AKF678', 'Education Support - Northern Youth Scholarships');
+    ('2023-2024', '2023-10-16', 'AZR123001', 800.00, 'Arizona Health Initiative', '123 Camelback Rd, Phoenix, AZ 85001', 'AZP789', 'AZA123, AZB456', 'Healthcare Access - Mobile Clinics'),
+    ('2023-2024', '2023-11-03', 'AZR123002', 400.00, 'Grand Canyon Conservation Society', '456 Sedona St, Flagstaff, AZ 86001', 'AZP234', 'AZC789, AZD012', 'Environmental Stewardship - Grand Canyon Preservation'),
+    ('2023-2024', '2023-11-09', 'AZR123003', 600.00, 'Phoenix Youth Empowerment', '789 Scottsdale Blvd, Tucson, AZ 85701', 'AZP567', 'AZE345, AZF678', 'Youth Development - Mentorship Programs');
+    ('2023-2024', '2023-10-17', 'ARR123001', 600.00, 'Arkansas Rural Outreach', '123 Little Rock Dr, Little Rock, AR 72201', 'ARP789', 'ARA123, ARB456', 'Rural Development - Community Gardens'),
+    ('2023-2024', '2023-11-04', 'ARR123002', 350.00, 'Ozark Wildlife Rehabilitation', '456 Fayetteville Ln, Fayetteville, AR 72701', 'ARP234', 'ARC789, ARD012', 'Wildlife Rehabilitation - Ozark Region'),
+    ('2023-2024', '2023-11-10', 'ARR123003', 750.00, 'Arkansas Education Foundation', '789 Jonesboro Ave, Jonesboro, AR 72401', 'ARP567', 'ARE345, ARF678', 'Education Access - School Supplies Drive');
+    ('2023-2024', '2023-10-11', 'R123456', 1000.00, 'American Charity Foundation', '123 Main St, City, State, 12345', 'ABC123', 'A123, B456', 'General Donation - Helping those in need'),
+    ('2023-2024', '2023-10-12', 'R123457', 750.00, 'United Relief Organization', '456 Oak St, Town, State, 67890', 'XYZ789', 'C789, D012', 'Emergency Fund - Disaster Relief'),
+    ('2023-2024', '2023-10-15', 'R123458', 500.00, 'Hope for Tomorrow', '789 Pine St, Village, State, 34567', 'PQR456', 'E345, F678', 'Education Support - Scholarships'),
+    ('2023-2024', '2023-11-01', 'R123459', 1200.00, 'Community Aid Foundation', '321 Elm St, Borough, State, 54321', 'LMN789', 'G901, H234', 'Healthcare Programs - Vaccination Drive'),
+    ('2023-2024', '2023-11-05', 'R123460', 200.00, 'Caring Hearts Initiative', '555 Maple St, Hamlet, State, 98765', 'STU012', 'I567, J890', 'Food Drive - Hunger Relief');
+    ('2022-2023', '2022-09-15', 'NHR123001', 500.00, 'Granite State Aid', '123 Concord Ave, Concord, NH 03301', 'NHP789', 'NHA123, NHB456', 'Community Support - Local Initiatives'),
+    ('2022-2023', '2022-10-10', 'NHR123002', 300.00, 'New Hampshire Nature Preservation', '456 Manchester Blvd, Manchester, NH 03101', 'NHP234', 'NHC789, NHD012', 'Nature Conservation - State Parks Preservation'),
+    ('2022-2023', '2022-11-05', 'NHR123003', 700.00, 'Education for All - NH', '789 Nashua St, Nashua, NH 03060', 'NHP567', 'NHE345, NHF678', 'Education Equality - Scholarships for All');
+    ('2022-2023', '2022-09-20', 'NJR123001', 800.00, 'Jersey Shore Community Outreach', '123 Atlantic Ave, Atlantic City, NJ 08401', 'NJP789', 'NJA123, NJB456', 'Coastal Community Support - Hurricane Relief'),
+    ('2022-2023', '2022-10-15', 'NJR123002', 400.00, 'Garden State Education Foundation', '456 Trenton St, Trenton, NJ 08601', 'NJP234', 'NJC789, NJD012', 'Education Access - School Infrastructure'),
+    ('2022-2023', '2022-11-10', 'NJR123003', 600.00, 'New Jersey Urban Renewal', '789 Newark Blvd, Newark, NJ 07101', 'NJP567', 'NJE345, NJF678', 'Community Revitalization - Urban Development Projects');
+    ('2022-2023', '2022-09-30', 'NYR123001', 700.00, 'Empire State Community Aid', '123 Broadway, New York, NY 10001', 'NYP789', 'NYA123, NYB456', 'Community Development - Affordable Housing Projects'),
+    ('2022-2023', '2022-10-25', 'NYR123002', 450.00, 'New York City Youth Foundation', '456 Wall St, Brooklyn, NY 11201', 'NYP234', 'NYC789, NYD012', 'Youth Empowerment - Mentorship Programs'),
+    ('2022-2023', '2022-11-20', 'NYR123003', 850.00, 'Big Apple Education Initiative', '789 Central Park West, Manhattan, NY 10001', 'NYP567', 'NYE345, NYF678', 'Education Equality - Scholarships for Underprivileged Students');
+    ('2022-2023', '2022-10-05', 'NCR123001', 550.00, 'Tar Heel State Health Foundation', '123 Raleigh Dr, Raleigh, NC 27601', 'NCP789', 'NCA123, NCB456', 'Healthcare Access - Rural Health Clinics'),
+    ('2022-2023', '2022-11-01', 'NCR123002', 400.00, 'North Carolina Environmental Alliance', '456 Charlotte Blvd, Charlotte, NC 28201', 'NCP234', 'NCC789, NCD012', 'Environmental Conservation - Forest Preservation'),
+    ('2022-2023', '2022-11-25', 'NCR123003', 650.00, 'Carolina Education for All', '789 Durham St, Durham, NC 27701', 'NCP567', 'NCE345, NCF678', 'Education Development - School Infrastructure Projects');
+    ('2022-2023', '2022-10-10', 'NDR123001', 500.00, 'Prairie State Community Support', '123 Bismarck Ave, Bismarck, ND 58501', 'NDP789', 'NDA123, NDB456', 'Community Aid - Local Initiatives'),
+    ('2022-2023', '2022-11-05', 'NDR123002', 350.00, 'Dakota Wildlife Conservation', '456 Fargo St, Fargo, ND 58101', 'NDP234', 'NDC789, NDD012', 'Wildlife Preservation - Prairie Ecosystem Conservation'),
+    ('2022-2023', '2022-11-30', 'NDR123003', 600.00, 'Education for the Dakotas', '789 Grand Forks Blvd, Grand Forks, ND 58201', 'NDP567', 'NDE345, NDF678', 'Education Access - Scholarships for Rural Students');
+    ('2023-2024', '2023-10-11', 'R123456', 1000.00, 'American Charity Foundation', '123 Main St, City, State, 12345', 'ABC123', 'A123, B456', 'General Donation - Helping those in need'),
+    ('2023-2024', '2023-10-12', 'R123457', 750.00, 'United Relief Organization', '456 Oak St, Town, State, 67890', 'XYZ789', 'C789, D012', 'Emergency Fund - Disaster Relief'),
+    ('2023-2024', '2023-10-15', 'R123458', 500.00, 'Hope for Tomorrow', '789 Pine St, Village, State, 34567', 'PQR456', 'E345, F678', 'Education Support - Scholarships'),
+    ('2023-2024', '2023-11-01', 'R123459', 1200.00, 'Community Aid Foundation', '321 Elm St, Borough, State, 54321', 'LMN789', 'G901, H234', 'Healthcare Programs - Vaccination Drive'),
+    ('2023-2024', '2023-11-05', 'R123460', 200.00, 'Caring Hearts Initiative', '555 Maple St, Hamlet, State, 98765', 'STU012', 'I567, J890', 'Food Drive - Hunger Relief');
