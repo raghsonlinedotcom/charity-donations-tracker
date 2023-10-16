6d44c15769f7a242158d599105f1b75f28edd29a
manikanta <manikanta@dataworkz.io>
2023-10-17 01:06:41 +0530

    [CDT-12] Repository Class and its methods

--- src/main/java/com/charity/charitydonationstracker/repository/CharityDonationsRepository.java
+++ src/main/java/com/charity/charitydonationstracker/repository/CharityDonationsRepository.java
@@ -4,12 +4,29 @@ import com.charity.charitydonationstracker.entity.CharityDonation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CharityDonationsRepository extends JpaRepository<CharityDonation, Long> {
 
-    public List<CharityDonation> findAllByFinancialYear(String financialYear);
+    List<CharityDonation> findAllByFinancialYear(String financialYear);
 
-    public List<CharityDonation> findAllByFinancialYearAndInstitution(String financialYear, String institution);
+    List<CharityDonation> findAllByFinancialYearAndInstitution(String financialYear, String institution);
+
+    List<CharityDonation> findByInstitution(String institution);
+
+    Optional<CharityDonation> findByPan(String pan);
+
+    List<CharityDonation> findByFinancialYear(String financialYear);
+
+    List<CharityDonation> findByReceipts(String Receipts);
+
+    List<CharityDonation> findByAddress(String address);
+
+    List<CharityDonation> findByDate(Date date);
+
+    List<CharityDonation> findByDateBetween(Date fromDate, Date toDate);
+
 }
