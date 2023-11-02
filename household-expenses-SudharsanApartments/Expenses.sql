2349c0f4e65f75954ae969c2c53425fd8e078976
Manikanta <manikantayarramsetti445@gmil.com>
2023-11-02 17:34:25 +0530

    inserting entries

--- household-expenses-SudharsanApartments/Expenses.sql
+++ household-expenses-SudharsanApartments/Expenses.sql
@@ -2,9 +2,21 @@
 --                  Expense Entries SQL 
 -- =====================================================
 
+
+-- -------------------
+-- 16 Oct 2023, Monday
+-- -------------------
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-16','15:02:00','Sun','DR','India','INR','Chennai','Water Can','Naresh Marketing RaviChandran','Indranagar, Adyar','Sudharsan Appartment', 'Day-Day','Water','Raghavan, Manikanta','Patym','40.00','Water Can @ Naresh Marketing RaviChandran', 'NULL', 'Day-Day, Water','0','0.0','2023-11-02 10:40:00','SYSTEM','2023-11-02 10:40:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-16','20:50:00','Sun','DR','India','INR','Chennai','Rapido Driver','City Office TRIL Campus','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Travelling','Raghavan','RaghsCanaraKKDI','126.00','Rapido Driver @ City Office TRIL Campus, Taramani', 'NULL', 'Day-Day, Office','0','0.0','2023-11-02 11:20:00','SYSTEM','2023-11-02 11:20:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-15','20:50:00','Sun','DR','India','INR','Chennai','Dosa Batter','Shri Krishna Foods','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Groceries','Raghavan, Manikanta','Cash','65.00','1Kg Dosa Batter, Flowers, fevistic @ Shri Krishna Foods Sudharsan Appartment Road Adyar', 'NULL', 'Day-Day, Food','1','0.0','2023-11-02 10:20:00','SYSTEM','2023-11-02 10:20:00','Manikanta');
+
 -- -------------------
 -- 15 Oct 2023, Sunday
 -- -------------------
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-15','20:37:00','Sun','DR','India','INR','Chennai','Reliance farm house','Reliance farm house','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Groceries','Raghavan, Manikanta','SodexhoMealCard','212.84','Rice 1kg, Pudina, Coriander 1 bunch each, Aavin Milk Orange, Heritage Curd, Aavin badam milk, Achi Semiya @ Reliance farm house Sudharsan Appartment Road Adyar', 'NULL', 'Day-Day, Food','0','0.0','2023-11-02 10:15:00','SYSTEM','2023-11-02 10:15:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-15','20:50:00','Sun','DR','India','INR','Chennai','Dosa Batter','Shri Krishna Foods','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Groceries','Raghavan, Manikanta','Cash','20.00','500 Grms Dosa Batter @ Shri Krishna Foods Sudharsan Appartment Road Adyar', 'NULL', 'Day-Day, Food','1','0.0','2023-11-02 10:20:00','SYSTEM','2023-11-02 10:20:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-15','21:12:00','Sun','DR','India','INR','Chennai','Ironing','IronShop','Indranagar, Adyar','Sudharsan Appartment', 'Day-Day','Ironing','Raghavan','Cash','20.00','3 clothes Rs 10 each, paid Rs 20 and adjusted Rs 10 excess in the previous time @ IronShop', 'NULL', 'Day-Day, Ironing','0','0.0','2023-11-02 10:25:00','SYSTEM','2023-11-02 10:25:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-15','21:30:00','Sun','DR','India','INR','Chennai','Raagi Puttu','Royal Puttu','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Food','Raghavan, Manikanta','Cash','50.00','Raagi Puttu 1 No @ Royal Puttu, Roadside shop Adyar', 'NULL', 'Day-Day, Food','1','0.0','2023-11-02 10:30:00','SYSTEM','2023-11-02 10:30:00','Manikanta');
 
 
 -- ---------------------
