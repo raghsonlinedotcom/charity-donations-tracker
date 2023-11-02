a3daa68e0aff028075322200277ad752483e1ec4
Manikanta <manikantayarramsetti445@gmil.com>
2023-11-02 21:41:31 +0530

    expenses

--- household-expenses-SudharsanApartments/Expenses.sql
+++ household-expenses-SudharsanApartments/Expenses.sql
@@ -3,12 +3,42 @@
 -- =====================================================
 
 
+-- 22 Oct 2023, Sunday
+-- -------------------
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-11-22','07:45:00','Sun','DR','India','INR','Chennai','Home','Native Cow Milk','Indranagar, Adyar','NULL', 'Day-Day','Groceries','Raghavan, Manikanta, Kiruba','Cash','80.00','1Ltr Native Milk Packet @ Native Milk, near Indranagar Signal, Adyar, Chennai', 'NULL', 'Day-Day, Groceries','0','0.0','2023-10-15 18:14:00','SYSTEM','2023-11-02 21:08:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-11-22','20:37:00','Sun','DR','India','INR','Chennai','Reliance farm house','Reliance farm house','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Groceries','Raghavan, Manikanta','SodexhoMealCard','240.76','2kg Onion, 1kg Tomato, Mushroom, capsicum, Heritage Curd @ Reliance farm house Sudharsan Appartment Road Adyar', 'NULL', 'Day-Day, Food','0','0.0','2023-11-02 10:15:00','SYSTEM','2023-11-02 10:15:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-11-22','07:45:00','Sun','DR','India','INR','Chennai','Theatre','Ganapathey Ram Theatre','Indranagar, Adyar','NULL', 'Day-Day','Entertainment','Raghavan, Manikanta, Kiruba','SBI Elite','296.20','Movie Tickets Booked Via Book My Show for Leo Movie @ Ganapathi Ram Nagar each ticket 190 Rs and Convenience fee 106 Rs , 2 Tkts waived Off for Using SBI Elite Card [SBI Elite]', 'NULL', 'Day-Day, Groceries','0','0.0','2023-10-15 18:14:00','SYSTEM','2023-11-02 21:08:00','Manikanta');
+
+
+-- 21 Oct 2023, Saturday
+-- -------------------
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-11-21','11:30:00','Sat','DR','India','INR','Chennai','IMeals','Roadside Shop','Indranagar Signal, Adyar','Close to the Petrol Bunk Adyar Road', 'Day-Day','Food','Raghavan, Manikanta','Cash','100.00','Lunch @ Roadside shop, near Indranagar Signal, Adyar - 1 full Meals and 1 Half Meals', 'NULL', 'Day-Day, Food','1','0.0','2023-10-15 18:10:00','SYSTEM','2023-10-15 18:10:00','Manikanta');
+
+-- -------------------
+-- 19 Oct 2023, Thursday
+-- -------------------
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-11-19','18:30:00','Thu','DR','India','INR','Chennai','Dosa Batter','Shri Krishna Foods','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Groceries','Raghavan, Manikanta','Cash','40.00','1Kg Dosa Batter @ Shri Krishna Foods Sudharsan Appartment Road Adyar', 'NULL', 'Day-Day, Food','1','0.0','2023-11-02 21:08:00','SYSTEM','2023-11-02 21:08:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-11-19','15:15:00','Thu','DR','India','INR','Chennai','Tea Shop','Tea Shop','Indranagar Signal, Adyar','NULL', 'Day-Day','Groceries','Raghavan, Manikanta','Cash','69.00','Aavin Milk Packet 1 No Blue Color, 1Ltr Curd Packet @ Tea shop, near Indranagar Signal, Adyar, Chennai', 'NULL', 'Day-Day, Groceries','1','0.0','2023-10-15 18:14:00','SYSTEM','2023-11-02 21:08:00','Manikanta');
+
+-- -------------------
+-- 18 Oct 2023, Wednesday
+-- -------------------
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-11-18','12:00:00','wed','DR','India','INR','Chennai','Rapido Driver','City Office TRIL Campus','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Travelling','Raghavan','RaghsCanaraKKDI','26.00','Rapido Driver @ City Office TRIL Campus, Taramani', 'NULL', 'Day-Day, Office','0','0.0','2023-11-02 21:08:00','SYSTEM','2023-11-02 21:08:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-11-18','15:00:00','wed','DR','India','INR','Chennai','Empty Boxes','Sold By Watchman','Indranagar, Adyar','Sudharsan Appartment', 'Day-Day','Items','Raghavan','Cash','170.00','Empty Boxes @ Sold By Watchman', 'NULL', 'Day-Day, Items','0','0.0','2023-11-02 21:08:00','SYSTEM','2023-11-02 21:08:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-11-18','18:30:00','wed','DR','India','INR','Chennai','Dosa Batter','Shri Krishna Foods','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Groceries','Raghavan, Manikanta','Cash','40.00','1Kg Dosa Batter @ Shri Krishna Foods Sudharsan Appartment Road Adyar', 'NULL', 'Day-Day, Food','1','0.0','2023-11-02 21:08:00','SYSTEM','2023-11-02 21:08:00','Manikanta');
+
+-- -------------------
+-- 17 Oct 2023, Tuesday
+-- -------------------
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-17','15:15:00','Tue','DR','India','INR','Chennai','Tea Shop','Tea Shop','Indranagar Signal, Adyar','NULL', 'Day-Day','Groceries','Raghavan, Manikanta','Cash','23.00','Aavin Milk Packet 1 No Blue Color @ Tea shop, near Indranagar Signal, Adyar, Chennai', 'NULL', 'Day-Day, Groceries','1','0.0','2023-10-15 18:14:00','SYSTEM','2023-11-02 21:08:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-17','15:15:00','Tue','DR','India','INR','Chennai','Masala Tea','Tea Shop','Indranagar Signal, Adyar','NULL', 'Day-Day','Beverages','Raghavan','RaghsCanaraKKDI','25.00','Masala Tea @ Tea shop, near Indranagar Road, Adyar, Chennai', 'NULL', 'Day-Day, Beverages','0','0.0','2023-10-15 18:14:00','SYSTEM','2023-11-02 21:08:00','Manikanta');
+
 -- -------------------
 -- 16 Oct 2023, Monday
 -- -------------------
-INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-16','15:02:00','Sun','DR','India','INR','Chennai','Water Can','Naresh Marketing RaviChandran','Indranagar, Adyar','Sudharsan Appartment', 'Day-Day','Water','Raghavan, Manikanta','Patym','40.00','Water Can @ Naresh Marketing RaviChandran', 'NULL', 'Day-Day, Water','0','0.0','2023-11-02 10:40:00','SYSTEM','2023-11-02 10:40:00','Manikanta');
-INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-16','20:50:00','Sun','DR','India','INR','Chennai','Rapido Driver','City Office TRIL Campus','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Travelling','Raghavan','RaghsCanaraKKDI','126.00','Rapido Driver @ City Office TRIL Campus, Taramani', 'NULL', 'Day-Day, Office','0','0.0','2023-11-02 11:20:00','SYSTEM','2023-11-02 11:20:00','Manikanta');
-INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-15','20:50:00','Sun','DR','India','INR','Chennai','Dosa Batter','Shri Krishna Foods','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Groceries','Raghavan, Manikanta','Cash','65.00','1Kg Dosa Batter, Flowers, fevistic @ Shri Krishna Foods Sudharsan Appartment Road Adyar', 'NULL', 'Day-Day, Food','1','0.0','2023-11-02 10:20:00','SYSTEM','2023-11-02 10:20:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-16','15:02:00','Mon','DR','India','INR','Chennai','Water Can','Naresh Marketing RaviChandran','Indranagar, Adyar','Sudharsan Appartment', 'Day-Day','Water','Raghavan, Manikanta','Patym','40.00','Water Can @ Naresh Marketing RaviChandran', 'NULL', 'Day-Day, Water','0','0.0','2023-11-02 10:40:00','SYSTEM','2023-11-02 10:40:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-16','20:50:00','Mon','DR','India','INR','Chennai','Rapido Driver','City Office TRIL Campus','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Travelling','Raghavan','RaghsCanaraKKDI','126.00','Rapido Driver @ City Office TRIL Campus, Taramani', 'NULL', 'Day-Day, Office','0','0.0','2023-11-02 11:20:00','SYSTEM','2023-11-02 11:20:00','Manikanta');
+INSERT INTO tblexpenses11jul19 VALUES (NULL, '2023-10-15','20:50:00','Mon','DR','India','INR','Chennai','Dosa Batter','Shri Krishna Foods','Indranagar, Adyar','Sudharsan Appartment Road', 'Day-Day','Groceries','Raghavan, Manikanta','Cash','65.00','1Kg Dosa Batter, Flowers, fevistic @ Shri Krishna Foods Sudharsan Appartment Road Adyar', 'NULL', 'Day-Day, Food','1','0.0','2023-11-02 10:20:00','SYSTEM','2023-11-02 10:20:00','Manikanta');
 
 -- -------------------
 -- 15 Oct 2023, Sunday
